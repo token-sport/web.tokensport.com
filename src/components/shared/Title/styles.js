9b08296c7a1e71f03542a752ea01e595c68ac76e
Esteban Aragon <esteban.aragonm@gmail.com>
2019-02-23 12:54:09 -0500

    Change in styles

--- src/components/shared/Title/styles.js
+++ src/components/shared/Title/styles.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 export const Text = styled.h1`
-   font-size: ${ ({ size, theme: { size: { font } } }) =>
-    size === 'xlg' ? font.xlg : size === 'lg' ? font.lg : font.sm};
+   font-size: ${ ({ size, theme: { size: { font } } }) => font[size]};
    font-family: ${ props => props.theme.family.nunito};
    color: white
 `
