779d6ed0f2b9431144e968efeff52eec7eafbbb0
Steven Anaya <stvanayar@gmail.com>
2019-02-22 19:50:58 -0500

    Creating initial Theme

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 // COMPONENTS
 import Title from 'components/shared/Title';
 
 const Home = () => (
    <Title>Stevene</Title>
-)
+);
 
-export default Home
+export default Home;
