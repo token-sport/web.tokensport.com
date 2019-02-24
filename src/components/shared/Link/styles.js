7b87e68a6cdd10c2e5676c84d9ed2184984fb053
Esteban Aragon <esteban.aragonm@gmail.com>
2019-02-23 22:41:30 -0500

    Link created

---
+++ src/components/shared/Link/styles.js
@@ -0,0 +1,11 @@
+export const LinkStyle = `
+  color: white;
+  font-family: 'Nunito';
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 100;
+  :hover {
+    color: #00E5FF;
+    text-decoration: underline;
+  }
+`
