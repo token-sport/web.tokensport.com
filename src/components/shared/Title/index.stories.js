f7ac7c93fbc2baa00ce81adf60de94cf543433bc
Esteban Aragon <esteban.aragonm@gmail.com>
2019-02-25 10:42:34 -0500

    Refactor to Title

--- src/components/shared/Title/index.stories.js
+++ src/components/shared/Title/index.stories.js
@@ -3,25 +3,25 @@ import { storiesOf } from '@storybook/react';
 import Title from './'
 
 // eslint-disable-next-line react/prop-types
-const Container = ({ children, size }) => (
+const Container = ({ children, size, uppercase, bold }) => (
   <div style={{ background: '#1d2837', padding: 10 }}>
-    <Title size={size}>{children}</Title>
+    <Title size={size} uppercase={uppercase} bold={bold}>{children}</Title>
   </div>
 )
 
 storiesOf('Title', module)
   .add('Large', () =>
-    <Container size="xlg">
+    <Container size="xlg" uppercase bold="800">
       Tokeniza tus emociones
     </Container>
   )
   .add('Medium', () =>
-    <Container size="lg">
+    <Container size="lg" bold="700">
       ¿Qué rayos es <span style={{ color: '#39B0E3' }}>Token Sport?</span>
     </Container>
   )
   .add('Small', () =>
-    <Container size="sm">
+    <Container size="sm" bold="600">
       Aduéñate de tus datos
     </Container>
   )
--- src/components/shared/Title/styles.js
+++ src/components/shared/Title/styles.js
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const Text = styled.h1`
    font-size: ${ ({ size, theme }) => theme.size.font[size]};
    font-family: ${ props => props.theme.family.nunito};
-   color: white
+   color: white;
+   text-transform: ${({ uppercase }) => uppercase ? 'uppercase' : 'normal'};
+   font-weight: ${({ bold }) => bold ? bold : 'normal'} ;
 `
