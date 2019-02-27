8344e1dd7fddcf77dfa339eda4e38a0a00005753
Esteban Aragon <esteban.aragonm@gmail.com>
2019-02-27 12:52:56 -0500

    Grid DreamTeam Created

--- src/components/LayoutHome/GridTeam/index.js
+++ src/components/LayoutHome/GridTeam/index.js
@@ -6,23 +6,31 @@ import Card from 'components/shared/Cards';
 import InfoUser from 'components/LayoutHome/InfoUser'
 
 // STYLES
-import { Container } from './styles'
+import { Container, Grid, SubTitle } from './styles'
+import Title from 'components/shared/Title';
+import Description from 'components/shared/Description';
 
 const GridTeam = ({ team }) => (
   <Container>
-    {
-      team.map((item, index) => (
-        <Card key={index} padding="40px" withGradient={false} showInHover>
-          <InfoUser
-            image={item.photo}
-            name={item.name}
-            profession={item.profession}
-            country={item.country}
-            shadow={item.color}
-          />
-        </Card>
-      ))
-    }
+    <Title size="xlg" bold="800">Dream Team</Title>
+    <Description size="xmd" color="white">
+      <SubTitle>Descubre el equipo titular de Token Sport</SubTitle>
+    </Description>
+    <Grid>
+      {
+        team.map((item, index) => (
+          <Card key={index} padding="40px" withGradient={false} showInHover>
+            <InfoUser
+              image={item.photo}
+              name={item.name}
+              profession={item.profession}
+              country={item.country}
+              shadow={item.color}
+            />
+          </Card>
+        ))
+      }
+    </Grid>
   </Container>
 )
 
--- src/components/LayoutHome/GridTeam/styles.js
+++ src/components/LayoutHome/GridTeam/styles.js
@@ -2,9 +2,18 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.background};
+  text-align: center;
+`
+
+export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, 400px);
   justify-content: center;
   justify-items: center;
-  padding: 50px 0px;
+  padding: 40px 0px;
+`
+
+export const SubTitle = styled.span`
+  width: 100%;
+  text-align: center;
 `
